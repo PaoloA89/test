3988f25dd476932e82f53b9414e3e3b46d4cb8bc
Paolo <p.ansuinelli@gmail.com>
2017-11-10 23:13:45 +0100

    added circumference function

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 print('Hello World')
-
+import numpy as np
 a = 53+47
 print(a)
+
+def circumference(radius):
+    b =2*np.pi*radius    
+    return b
+c = float(input("Dammi in ingresso valore raggio: "))
+print(circumference(c))
+
+
+
