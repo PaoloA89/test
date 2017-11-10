02bda7c9aaf456175a84a022b5240823c9e4f18a
Paolo <p.ansuinelli@gmail.com>
2017-11-10 23:25:24 +0100

    added function circle_area

--- test.py
+++ test.py
@@ -6,8 +6,10 @@ print(a)
 def circumference(radius):
     b =2*np.pi*radius    
     return b
+def circle_area(radius):
+    return np.pi*radius**2
 c = float(input("Dammi in ingresso valore raggio: "))
-print(circumference(c))
+print(circumference(c),circle_area(c))
 
 
 
